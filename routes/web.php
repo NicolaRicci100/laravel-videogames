a0adf9bee3b65a9a022ea2de59ee5a972caf20ef
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-08 12:00:33 +0200

    added platform crud and msrc

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::resource('admin/platforms', App\Http\Controllers\Admin\PlatformController::class, ['as' => 'admin']);
