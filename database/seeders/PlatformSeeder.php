d9e709b6fc43f30fe2502604de063bcc74b97bf8
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-08 12:20:10 +0200

    added seeder for table platform

--- database/seeders/PlatformSeeder.php
+++ database/seeders/PlatformSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Platform;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class PlatformSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $platforms = [
+            ['name' => 'PS4', 'color' => 'primary'],
+            ['name' => 'PS5', 'color' => 'light'],
+            ['name' => 'Xbox', 'color' => 'success'],
+            ['name' => 'PC', 'color' => 'warning'],
+            ['name' => 'Switch', 'color' => 'danger'],
+        ];
+
+        foreach ($platforms as $platform) {
+            $newPlatform = new Platform();
+            $newPlatform->name = $platform['name'];
+            $newPlatform->color = $platform['color'];
+            $newPlatform->save();
+        }
     }
 }
